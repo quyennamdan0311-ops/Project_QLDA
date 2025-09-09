73a79e6e616f452a8ac16469fa81a6cf29aec350
manh <manhtran171205@gmail.com>
2025-09-09 17:15:56 +0700

    manh space

---
+++ manhproject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_QLDA
+{
+    public partial class manhproject : Form
+    {
+        public manhproject()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
